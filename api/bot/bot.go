53ab3bf203cd9f00fe89f2d63d636790bd7c4557
Mike Blum <me+github@mblum.me>
2023-01-16 14:47:09 -0600

    tests: refactor ping tests

--- api/bot/bot.go
+++ api/bot/bot.go
@@ -3,23 +3,15 @@ package bot
 import (
 	"net/http"
 
-	"google.golang.org/protobuf/reflect/protoreflect"
-	buf "teapotbot.dev/api/internal/ping"
 	"teapotbot.dev/api/transport"
 )
 
 type Bot struct {
-	*buf.PingRequest
 	*http.Client
 }
 
 func New() *Bot {
 	return &Bot{
-		nil,
 		transport.New().Client(),
 	}
-}
-
-func (b *Bot) Proto() protoreflect.Message {
-	return b.ProtoReflect()
 }
--- api/ping/ping_test.go
+++ api/ping/ping_test.go
@@ -8,7 +8,6 @@ import (
 	"github.com/stretchr/testify/mock"
 	"google.golang.org/protobuf/proto"
 	"google.golang.org/protobuf/types/known/durationpb"
-	bot "teapotbot.dev/api/bot"
 	buf "teapotbot.dev/api/internal/ping"
 )
 
@@ -21,14 +20,12 @@ func (m *mockedPingRequest) Ping() (*buf.PingResponse, error) {
 	return nil, args.Error(1)
 }
 
-func defaultBot() *bot.Bot {
-	defaultBot := bot.New()
-	defaultBot.PingRequest = &buf.PingRequest{
+func defaultPing() *buf.PingRequest {
+	return &buf.PingRequest{
 		Method:  buf.Method_GET,
 		Url:     &buf.Url{Host: "localhost:80"},
 		Timeout: &durationpb.Duration{Seconds: 10},
 	}
-	return defaultBot
 }
 
 func TestNewPingRequestMock(t *testing.T) {
@@ -41,21 +38,21 @@ func TestNewPingRequestMock(t *testing.T) {
 }
 
 func TestNewPingRequestDefaults(t *testing.T) {
-	req := defaultBot()
+	req := defaultPing()
 	assert.Equal(t, buf.Scheme_TCP, req.Url.Scheme, "defaults to tcp")
 	assert.Equal(t, buf.Method_GET, req.Method, "defaults to GET")
 }
 
 func TestNewPingRequestProto(t *testing.T) {
 	t.Run("serialize PingRequest", func(t *testing.T) {
-		req := defaultBot()
-		dat, err := proto.Marshal(req.Proto().Interface())
+		req := defaultPing()
+		dat, err := proto.Marshal(req)
 		assert.Nil(t, err)
 		assert.True(t, len(dat) > 0)
 	})
 	t.Run("deserialize PingRequest", func(t *testing.T) {
-		req := defaultBot()
-		dat, err := proto.Marshal(req.Proto().Interface())
+		req := defaultPing()
+		dat, err := proto.Marshal(req)
 		assert.Nil(t, err)
 		expected := &buf.PingRequest{}
 		err = proto.Unmarshal(dat, expected.ProtoReflect().Interface())
